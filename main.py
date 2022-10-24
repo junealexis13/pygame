7e87aead03ecb8f3204c693904dff171e769f555
junealexis13 <junealexis.santos13@gmail.com>
2022-10-24 13:15:31 +0800

    added drawing rectangle

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from cgi import test
-from platform import python_branch
-from xml.dom import minidom
 import pygame
 import sys
 
@@ -11,6 +8,7 @@ width = 800
 height = 400
 screen = pygame.display.set_mode((width,height))                                    #it sets the screen up
 
+score = 0
 pygame.display.set_caption('Runner')                                                #the game title
 
 #creating a font object
@@ -23,23 +21,24 @@ clock = pygame.time.Clock()
 framerate = 60                                                                      #we are setting the FR to 60fps
 
 # test_surface = pygame.Surface((100, 200))                                         #Test surface
-sky_surface = pygame.image.load('graphics/sky.png').convert()                                 #Importing the Image as a surface
-ground_surface = pygame.image.load('graphics/ground.png').convert()     
+sky_surf = pygame.image.load('graphics/sky.png').convert()                                 #Importing the Image as a surface
+ground_surf = pygame.image.load('graphics/ground.png').convert()     
 # test_surface.fill('Red')
 
 #Text font surface
-text_surface = test_font.render('My game', False, 'Green')
+score_surf = test_font.render("My Game", False, 'Green')
+score_rect = score_surf.get_rect( center = (400, 50))                                   #the rect was used as a placeholder
 
 #importing moving snail and ANIMATION 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()              #convert_alpha get rid of black and white stuff
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()              #convert_alpha get rid of black and white stuff
 snail_xpos = 600
 
-snail_rectangle = snail_surface.get_rect(midbottom = (snail_xpos, height - 100))
+snail_rectangle = snail_surf.get_rect(midbottom = (snail_xpos, height - 100))
 
 #creating a player surface  -WORKING WITH RECTANGLES
-player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 # player_rectange = pygame.Rect()                                                     #Creates a rectangle Object this accepts(left, top, width, height)
-player_rect = player_surface.get_rect(midbottom= (80,height - 100))                             #this creates a rectangle based on sprite dimension  
+player_rect = player_surf.get_rect(midbottom= (80,height - 100))                             #this creates a rectangle based on sprite dimension  
 #the get_rect acts like an ANCHORPOINT
 #can be substttd with top-mid-bottom + left-right
 
@@ -53,7 +52,7 @@ while True:                                                                     
             #the while loop terminates here with sys.exit()
             #clicking the close window will exit the code
 
-        # #SOME MOUSE EVENTS
+        # # #SOME MOUSE EVENTS
         # if event.type == pygame.MOUSEMOTION:     #Detects wether the mouse cursor moves
         #     if player_rect.collidepoint(event.pos):             # event pos returns the mouse position
         #         print('COLLIDE!')                         
@@ -63,19 +62,21 @@ while True:                                                                     
         #     print("Click Releeased!")
 
     # screen.blit(test_surface, (0,0))                                               #showing surface
-    screen.blit(sky_surface, (0,0))                                                  #showing sky surface        the tuple goes like (X,Y)
-    screen.blit(ground_surface,(0,height - 100) )                                    #showing ground surface
-    screen.blit(text_surface, (325, 50))
+    screen.blit(sky_surf, (0,0))                                                  #showing sky surface        the tuple goes like (X,Y)
+    screen.blit(ground_surf,(0,height - 100) )                                    #showing ground surface
+    pygame.draw.rect(screen, 'Pink', score_rect, 5, 10)                              #surface, color, position, width,  
+    screen.blit(score_surf, score_rect)
 
     #animating snail
     if snail_rectangle.left <= width - (width * 1.10):
         snail_rectangle.left = width + 100
+        score += 1
         
     snail_rectangle.left += -2  
-    screen.blit(snail_surface, snail_rectangle)
+    screen.blit(snail_surf, snail_rectangle)
     
     #adding the player sprite
-    screen.blit(player_surface, player_rect)
+    screen.blit(player_surf, player_rect)
     player_rect.left += 2
     if player_rect.left >= width + (width * 0.10):
         player_rect.left = -100                                                 #moving the rectangle that contains the object moves it also
